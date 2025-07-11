57f5617386ad23d5fe4b25625454aa581042dd86
DinhHieu2506 <hieucr78@gmail.com>
2025-07-11 11:36:16 +0700

    fix: standardize import statement formatting in FilterButtons component

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setFilter } from '../features/todos/TodosSlice.jsx'
+import { setFilter } from "../features/todos/TodosSlice.jsx"
 import { Segmented } from 'antd'
 
 const FilterButtons = () => {
